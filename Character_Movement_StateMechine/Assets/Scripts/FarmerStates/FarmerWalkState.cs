6ef87aaa2448d0852bebf121af3d596ca7ae70d4
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2024-02-05 19:38:19 +0600

    Update FarmerWalkState.cs

--- Character_Movement_StateMechine/Assets/Scripts/FarmerStates/FarmerWalkState.cs
+++ Character_Movement_StateMechine/Assets/Scripts/FarmerStates/FarmerWalkState.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FarmerWalkState : FarmerBaseState
 {
@@ -33,35 +36,134 @@ public class FarmerWalkState : FarmerBaseState
     {
         if (collider.CompareTag(KeyManager.TagInventory) && farmer.farmerAction is FarmerActions.Seeding)
         {
-            farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
-            farmer.SwitchState(farmer.boxPickupState);
+            // farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
+            var targetTrans = collider.transform.GetChild(0).transform;
+            
+            StartCoroutine(GetToPositionAndRotation(
+                targetTrans.position,
+                targetTrans.rotation,
+                0.3f,
+                (success) =>
+                    {
+                        if (success)
+                        {
+                            farmer.SwitchState(farmer.seedingState);
+                        }
+                    }
+                ));
+            
+            // farmer.SwitchState(farmer.boxPickupState);
         }
         if (collider.CompareTag(KeyManager.TagField) && farmer.farmerAction is FarmerActions.Watering)
         {
-            farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
-            farmer.SwitchState(farmer.wateringState);
+            // farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
+            var targetTrans = collider.transform.GetChild(0).transform;
+            
+            StartCoroutine(GetToPositionAndRotation(
+                targetTrans.position,
+                targetTrans.rotation,
+                3f,
+                (success) =>
+                    {
+                        if (success)
+                        {
+                            farmer.SwitchState(farmer.wateringState);
+                        }
+                    }
+                ));
+            
+            // farmer.SwitchState(farmer.wateringState);
         }
         if (collider.CompareTag(KeyManager.TagField) && farmer.farmerAction == FarmerActions.Harvesting)
         {
-            farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
-            farmer.SwitchState(farmer.harvestingState);
+            // farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
+            var targetTrans = collider.transform.GetChild(0).transform;
+            
+            StartCoroutine(GetToPositionAndRotation(
+                targetTrans.position,
+                targetTrans.rotation,
+                0.3f,
+                (success) =>
+                    {
+                        if (success)
+                        {
+                            farmer.SwitchState(farmer.harvestingState);
+                        }
+                    }
+                ));
+            
         }
         if (collider.CompareTag(KeyManager.TagField) && farmer.farmerAction == FarmerActions.Building)
         {
-            farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
-            farmer.SwitchState(farmer.kneelDownState);
+            // farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
+            var targetTrans = collider.transform.GetChild(0).transform;
+            
+            StartCoroutine(GetToPositionAndRotation(
+                targetTrans.position,
+                targetTrans.rotation,
+                0.3f,
+                (success) =>
+                {
+                    if (success)
+                    {
+                        farmer.SwitchState(farmer.kneelDownState);
+                    }
+                }));
+            
+            
         }
         if (collider.CompareTag(KeyManager.TagInventory) || collider.CompareTag(KeyManager.TagField) && farmer.farmerAction is FarmerActions.Carry)
         {
-            farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
-            farmer.SwitchState(farmer.boxPickupState);
+            // farmer.SetPositionAndRotation(collider.transform.GetChild(0).transform);
+            var targetTrans = collider.transform.GetChild(0).transform;
+            
+            StartCoroutine(GetToPositionAndRotation(
+                targetTrans.position,
+                targetTrans.rotation,
+                0.3f,
+                (success) =>
+                {
+                    if (success)
+                    {
+                        farmer.SwitchState(farmer.boxPickupState);
+                    }
+                }));
         }
 
         if (collider.CompareTag(KeyManager.TagTree) && farmer.farmerAction is FarmerActions.CuttingTree)
         {
-            farmer.transform.position = collider.GetComponent<Tree>().ClosestStandPosition(transform.position).position;
-            farmer.transform.rotation = collider.GetComponent<Tree>().ClosestStandPosition(transform.position).rotation;
-            farmer.SwitchState(farmer.cutTreeState);
+            var closestStandPosition = collider.GetComponent<Tree>().ClosestStandPosition(transform.position);
+            StartCoroutine(GetToPositionAndRotation(
+                closestStandPosition.position, 
+                closestStandPosition.rotation, 
+                0.3f,
+                (bool success) =>
+                {
+                    if (success)
+                    {
+                        farmer.SwitchState(farmer.cutTreeState);
+                    }
+                }));
+            
+            
         }
     }
-}
+
+    private IEnumerator GetToPositionAndRotation(Vector3 pos, Quaternion rot, float time, Action<bool> callBack)
+    {
+        float t = 0;
+        var position = transform.position;
+        var rotation = transform.rotation;
+        while (t < time)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(position, pos, t / time);
+            transform.rotation = Quaternion.Lerp(rotation, rot, t / time);
+            yield return null;
+        }
+        callBack(true);
+        yield break;
+    }
+    
+
+} // class
